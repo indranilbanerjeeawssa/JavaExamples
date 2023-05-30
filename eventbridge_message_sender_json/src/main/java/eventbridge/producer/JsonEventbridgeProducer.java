bdf19d709096cd2003498a7522c76239d9a1bd4f
indranilbanerjeeawssa <indranilawssfo10@gmail.com>
2023-05-30 01:24:13 -0700

    Fixed Eventbridge sender to remove SNS references

--- eventbridge_message_sender_json/src/main/java/eventbridge/producer/JsonEventbridgeProducer.java
+++ eventbridge_message_sender_json/src/main/java/eventbridge/producer/JsonEventbridgeProducer.java
@@ -1,9 +1,7 @@
 package eventbridge.producer;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,19 +9,11 @@ import java.io.InputStreamReader;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
 import software.amazon.awssdk.services.eventbridge.EventBridgeClient;
 import software.amazon.awssdk.services.eventbridge.model.PutEventsRequest;
 import software.amazon.awssdk.services.eventbridge.model.PutEventsRequestEntry;
 import software.amazon.awssdk.services.eventbridge.model.PutEventsResponse;
-import software.amazon.awssdk.services.sns.SnsClient;
-import software.amazon.awssdk.services.sns.model.ListTopicsRequest;
-import software.amazon.awssdk.services.sns.model.ListTopicsResponse;
-import software.amazon.awssdk.services.sns.model.PublishRequest;
-import software.amazon.awssdk.services.sns.model.PublishResponse;
-import software.amazon.awssdk.services.sns.model.SnsException;
-import software.amazon.awssdk.services.sns.model.Topic;
-import software.amazon.awssdk.services.sns.model.MessageAttributeValue;
+
 
 public class JsonEventbridgeProducer {
 	
@@ -77,74 +67,9 @@ public class JsonEventbridgeProducer {
             System.out.println("Now done sending one Eventbridge message");
             System.out.println("**********************************************************");
 
-         } catch (SnsException e) {
-            System.err.println(e.awsErrorDetails().errorMessage());
-            System.exit(1);
-         }
-    }
-	
-	public static void snsSender(String snsTopic, String messageKey, int numberOfMessages) {
-		SnsClient snsClient = SnsClient.builder().build();
-		String snsTopicARN = JsonEventbridgeProducer.getTopicARNFromTopicName(snsClient, snsTopic);
-		List<String> people = JsonEventbridgeProducer.readDataFile(); 
-		 int numberOfMessagesToSend=0; 
-		 if (people.size() > numberOfMessages) { 
-			 numberOfMessagesToSend = numberOfMessages; 
-		 } else { 
-			 numberOfMessagesToSend = people.size(); 
-		 }
-		for (int i=1;i<= numberOfMessagesToSend; i++) {
-			Person thisPerson = JsonEventbridgeProducer.getPersonFromLine(people.get(i));
-			JsonEventbridgeProducer.sendMessage(snsClient, snsTopicARN, thisPerson.toJson(), messageKey, i);
-		}
-	}
-	
-	public static String getTopicARNFromTopicName(SnsClient snsClient, String topicName) {
-		String topicARN = "";
-		try {
-            ListTopicsRequest request = ListTopicsRequest.builder()
-                   .build();
-
-            ListTopicsResponse result = snsClient.listTopics(request);
-            List<Topic> topics = result.topics();
-            for (Topic topic: topics) {
-            	String currentTopicARN = topic.topicArn();
-            	if (currentTopicARN.endsWith(topicARN)) {
-            		topicARN = currentTopicARN;
-            		break;
-            	}
-            }
-
-        } catch (SnsException e) {
-        	System.err.println("Incorrect Topic Name Specified");
-            System.err.println(e.awsErrorDetails().errorMessage());
-            System.exit(1);
-        }
-		return topicARN;
-	}
-	
-	public static void sendMessage(SnsClient snsClient, String topicARN, String message, String messageKey, int messageNumber) {
-		try {
-			Map<String, MessageAttributeValue> attributes = new HashMap<String, MessageAttributeValue>();
-            attributes.put("MessageKey", MessageAttributeValue.builder().dataType("String").stringValue(messageKey).build());
-            attributes.put("MessageNumber", MessageAttributeValue.builder().dataType("String").stringValue(Integer.toString(messageNumber)).build());
-            PublishRequest request = PublishRequest.builder()
-                .message(message)
-                .topicArn(topicARN)
-                .messageAttributes(attributes)
-                .subject("Sending Message with Key = " + messageKey + " and message number = " + Integer.toString(messageNumber))
-                .build();
-            System.out.println("**********************************************************");
-            System.out.println("Now going to send one SNS message to topic - " + topicARN);
-            System.out.println("Message Key = " + messageKey + " and Message Number = " + Integer.toString(messageNumber));
-            System.out.println("Message Body = " + message);
-            PublishResponse result = snsClient.publish(request);
-            System.out.println(result.messageId() + " Message sent. Status is " + result.sdkHttpResponse().statusCode());
-            System.out.println("Now done sending one SNS message");
-            System.out.println("**********************************************************");
-
-         } catch (SnsException e) {
-            System.err.println(e.awsErrorDetails().errorMessage());
+         } catch (Exception e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
             System.exit(1);
          }
     }
