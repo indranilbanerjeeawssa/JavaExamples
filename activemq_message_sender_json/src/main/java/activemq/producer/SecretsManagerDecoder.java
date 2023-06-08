57bbdd3abdbc856de7a8ae4ad4d6a87b04b33e91
indranilbanerjeeawssa <indranilawssfo10@gmail.com>
2023-06-08 00:40:49 -0700

    Added ActiveMQ Sender Incomplete Code

--- activemq_message_sender_json/src/main/java/activemq/producer/SecretsManagerDecoder.java
+++ activemq_message_sender_json/src/main/java/activemq/producer/SecretsManagerDecoder.java
@@ -1,22 +1,16 @@
 package activemq.producer;
 
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.regions.providers.DefaultAwsRegionProviderChain;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
 
+
 public class SecretsManagerDecoder {
 	
-	public static void getSecret() {
+	public static String getSecret() {
 
 		String secretName = "AmazonMQCredentials";
-	    //Region region = Region.of("us-west-2");
-
-		
-		
-	    // Create a Secrets Manager client
 	    SecretsManagerClient client = SecretsManagerClient.builder()
 	    		.credentialsProvider(DefaultCredentialsProvider.builder().build())
 	            .build();
@@ -25,21 +19,21 @@ public class SecretsManagerDecoder {
 	            .secretId(secretName)
 	            .build();
 
-	    GetSecretValueResponse getSecretValueResponse;
+	    GetSecretValueResponse getSecretValueResponse = null;
 
 	    try {
 	        getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
 	    } catch (Exception e) {
-	        // For a list of exceptions thrown, see
-	        // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-	        throw e;
+	        e.printStackTrace();
 	    }
-
-	    String secret = getSecretValueResponse.secretString();
-	    
+	    if (null != getSecretValueResponse) {
+	    	return getSecretValueResponse.secretString();
+	    } else {
+	    	return "Sorry mate! No secret found";
+	    }
 	}
 	
 	public static void main(String[] args) {
-		SecretsManagerDecoder.getSecret();
+		System.out.println(SecretsManagerDecoder.getSecret());
 	}
 }
