46b125b7688e9090c2840401ebafa4b4ccad9606
indranilbanerjeeawssa <indranilawssfo10@gmail.com>
2023-07-12 06:54:58 -0700

    Fixed API Gateway Sender code

--- apigateway_restapi_nonproxy_message_sender_json/src/main/java/apigateway/rest/nonproxy/producer/JsonAPIGatewayRESTProducer.java
+++ apigateway_restapi_nonproxy_message_sender_json/src/main/java/apigateway/rest/nonproxy/producer/JsonAPIGatewayRESTProducer.java
@@ -1,9 +1,7 @@
 package apigateway.rest.nonproxy.producer;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,24 +10,45 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 
 public class JsonAPIGatewayRESTProducer {
-	
+
 	public static void main(String[] args) {
 		try {
-			JsonAPIGatewayRESTProducer.restAPISender(args[0], args[1].concat(JsonAPIGatewayRESTProducer.getTodayDate()), Integer.parseInt(args[2]));
+			JsonAPIGatewayRESTProducer.restAPISender(args[0], args[1].concat(JsonAPIGatewayRESTProducer.getTodayDate()),
+					Integer.parseInt(args[2]));
 		} catch (NumberFormatException e) {
-			System.out.println("Pass three parameters: 1 - API URL, 2 - A string to be used as key for this batch of messages, 3 - Number of Messages in this batch");
+			System.out.println(
+					"Pass three parameters: 1 - API URL, 2 - A string to be used as key for this batch of messages, 3 - Number of Messages in this batch");
 			e.printStackTrace();
 		}
 	}
-	
 
 	public static void restAPISender(String apiGatewayURL, String messageKey, int numberOfMessages) {
-		
+
+		try {
+
+			List<String> people = JsonAPIGatewayRESTProducer.readDataFile();
+			int numberOfMessagesToSend = 0;
+			if (people.size() > numberOfMessages) {
+				numberOfMessagesToSend = numberOfMessages;
+			} else {
+				numberOfMessagesToSend = people.size();
+			}
+			for (int i = 1; i <= numberOfMessagesToSend; i++) {
+				Person thisPerson = JsonAPIGatewayRESTProducer.getPersonFromLine(people.get(i));
+				PersonWithID thisPersonWithID = new PersonWithID(thisPerson, messageKey + "-" + i);
+				JsonAPIGatewayRESTProducer.sendRestPostMessage(apiGatewayURL, thisPersonWithID);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	public static void sendRestPostMessage(String apiGatewayURL, PersonWithID personWithID) {
+
 		URL url = null;
 		HttpURLConnection conn = null;
 		OutputStream os = null;
@@ -40,62 +59,33 @@ public class JsonAPIGatewayRESTProducer {
 			conn.setRequestMethod("POST");
 			conn.setRequestProperty("Content-Type", "application/json");
 			os = conn.getOutputStream();
-			List<String> people = JsonAPIGatewayRESTProducer.readDataFile(); 
-			 int numberOfMessagesToSend=0; 
-			 if (people.size() > numberOfMessages) { 
-				 numberOfMessagesToSend = numberOfMessages; 
-			 } else { 
-				 numberOfMessagesToSend = people.size(); 
-			 }
-			for (int i=1;i<= numberOfMessagesToSend; i++) {
-				Person thisPerson = JsonAPIGatewayRESTProducer.getPersonFromLine(people.get(i));
-				PersonWithID thisPersonWithID = new PersonWithID(thisPerson, messageKey + "-" + i);
-				JsonAPIGatewayRESTProducer.sendRestPostMessage(conn, os, thisPersonWithID);
+
+			String input = personWithID.toJson();
+			System.out.println("**********************************************************");
+			System.out.println("Now going to send one POST message");
+			System.out.println("Message Body = " + input);
+			os.write(input.getBytes());
+			os.flush();
+			if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
 			}
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+			String output;
+			System.out.println("Output from Server = \n");
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+			System.out.println("Now done sending one POST message");
+			System.out.println("**********************************************************");
 			os.close();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-			System.exit(1);
-		} catch (ProtocolException e) {
-			e.printStackTrace();
-			System.exit(1);
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-		} finally {
 			conn.disconnect();
+		} catch (Exception e) {
+			System.err.println(e);
+			System.exit(1);
 		}
 	}
-	
-	public static void sendRestPostMessage(HttpURLConnection conn, OutputStream os, PersonWithID personWithID) {
-        try {
-        	String input = personWithID.toJson();
-            System.out.println("**********************************************************");
-            System.out.println("Now going to send one POST message");
-            System.out.println("Message Body = " + input);
-            os.write(input.getBytes());
-    		os.flush();
-            if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
-    			throw new RuntimeException("Failed : HTTP error code : "
-    				+ conn.getResponseCode());
-    		}
-    		BufferedReader br = new BufferedReader(new InputStreamReader(
-    				(conn.getInputStream())));
 
-    		String output;
-    		System.out.println("Output from Server = \n");
-    		while ((output = br.readLine()) != null) {
-    			System.out.println(output);
-    		}
-            System.out.println("Now done sending one POST message");
-            System.out.println("**********************************************************");
-
-        } catch (Exception e) {
-            System.err.println(e);
-            System.exit(1);
-        }
-    }
-	
 	public static List<String> readDataFile() {
 		List<String> personList = new ArrayList<String>();
 		InputStream is = JsonAPIGatewayRESTProducer.class.getClassLoader().getResourceAsStream("us-500.csv");
@@ -112,7 +102,7 @@ public class JsonAPIGatewayRESTProducer {
 		}
 		return personList;
 	}
-	
+
 	public static Person getPersonFromLine(String line) {
 		String[] fields = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
 		Person thisPerson = new Person();
@@ -130,12 +120,12 @@ public class JsonAPIGatewayRESTProducer {
 		thisPerson.setWebsite(fields[11]);
 		return thisPerson;
 	}
-	
+
 	public static String getTodayDate() {
-		
+
 		LocalDateTime ldt = LocalDateTime.now();
-        String formattedDateStr = DateTimeFormatter.ofPattern("MM-dd-YYYY-HH-MM-SS").format(ldt);
-        return formattedDateStr;
+		String formattedDateStr = DateTimeFormatter.ofPattern("MM-dd-YYYY-HH-MM-SS").format(ldt);
+		return formattedDateStr;
 	}
 
 }
